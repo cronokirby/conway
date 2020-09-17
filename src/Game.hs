99841d6cdf6e4c6033d7433fe13d6ce36590291b
Lúcás Meier <cronokirby@gmail.com>
2020-09-17 15:25:22 +0200

    Add cursor for drawing stuff onto grid

--- src/Game.hs
+++ src/Game.hs
@@ -3,6 +3,7 @@
 
 module Game (startGame) where
 
+import Life (Pos)
 import SDL
 import Prelude
 
@@ -16,7 +17,7 @@ startGame = do
   runGame s (loop ts)
 
 -- An action that the user can take
-data Action = EscapeAction | PauseAction | NoAction
+data Action = EscapeAction | PauseAction | NoAction | HoverAction Pos
 
 eventToAction :: Event -> Action
 eventToAction event = case eventPayload event of
@@ -24,12 +25,15 @@ eventToAction event = case eventPayload event of
     KeycodeEscape -> EscapeAction
     KeycodeSpace -> PauseAction
     _ -> NoAction
+  MouseMotionEvent e ->
+    let (P (V2 x y)) = mouseMotionEventPos e
+     in HoverAction (fromIntegral (x `div` 20), fromIntegral (y `div` 20))
   _ -> NoAction
 
-data GameState = GameState {paused :: Bool, shouldQuit :: Bool, rdr :: Renderer, timeBuf :: Word32, flp :: Bool}
+data GameState = GameState {paused :: Bool, shouldQuit :: Bool, rdr :: Renderer, timeBuf :: Word32, hover :: Pos}
 
 initialGameState :: Renderer -> GameState
-initialGameState rdr' = GameState True False rdr' 0 False
+initialGameState rdr' = GameState True False rdr' 0 (0, 0)
 
 newtype GameM a = GameM (ReaderT (IORef GameState) IO a) deriving (Functor, Applicative, Monad, MonadReader (IORef GameState), MonadIO)
 
@@ -54,21 +58,34 @@ loop ts = do
   newTs <- SDL.ticks
   unlessM (gets paused) (modify (\s -> s {timeBuf = (timeBuf s) + (newTs - ts)}))
   handleTime
-  color <- ifM (gets flp) (return (V4 0 0 255 255)) (return (V4 0 255 0 255))
-  rendererDrawColor rdr' $= color
+  rendererDrawColor rdr' $= V4 0 0 0 255
   clear rdr'
+  hvr' <- gets hover
+  drawSquareOutline (V4 0 0 255 255) hvr'
   present rdr'
   unlessM (gets shouldQuit) (loop newTs)
+
+rectFromPos :: Num a => Pos -> Rectangle a
+rectFromPos (x, y) =
+  let p = P (V2 (fromIntegral (x * 20)) (fromIntegral (y * 20)))
+   in Rectangle p (V2 20 20)
+
+drawSquareOutline :: V4 Word8 -> Pos -> GameM ()
+drawSquareOutline color pos = do
+  rdr' <- gets rdr
+  rendererDrawColor rdr' $= color
+  drawRect rdr' (Just (rectFromPos pos))
 
 handleAction :: Action -> GameM ()
 handleAction a = case a of
   EscapeAction -> modify (\s -> s {shouldQuit = True})
   PauseAction -> modify (\s -> s {paused = not (paused s)})
+  HoverAction pos -> modify (\s -> s {hover = pos})
   NoAction -> return ()
 
 handleTime :: GameM ()
 handleTime = do
   tm <- gets timeBuf
   if tm > 500
-    then modify (\s -> s {timeBuf = 0, flp = not (flp s)})
+    then modify (\s -> s {timeBuf = 0})
     else return ()
